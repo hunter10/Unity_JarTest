fea12ad6c92681df04f90bd36d9db2e1353c24b8
DESKTOP-4TAOHB8\hunter <hunter10@naver.com>
2018-01-03 01:01:29 +0900

    매니페스트만 해결되면 다 될거 같음

--- AndroidStudio/myplugin/src/main/java/com/test/myplugin/MyPluginActivity.java
+++ AndroidStudio/myplugin/src/main/java/com/test/myplugin/MyPluginActivity.java
@@ -8,9 +8,21 @@ import com.unity3d.player.UnityPlayerActivity;
 
 public class MyPluginActivity extends UnityPlayerActivity
 {
-    public static int GetStaticInt(int a) {return a+a;}
-    public static String GetStaticString(String str) {return "Static GetString() : " + str;}
-    public int GetInt(int a){return a+a;}
+    public static int GetStaticInt(int a)
+    {
+        return a + a;
+    }
+
+    public static String GetStaticString(String str)
+    {
+        return "Static GetString() : " + str;
+    }
+
+    public int GetInt(int a)
+    {
+        return a + a;
+    }
+
     public String GetString(String str)
     {
         return "GetString() : " + str;
